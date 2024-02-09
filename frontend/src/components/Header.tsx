201a269d055ca96df0fe5692e82e03cc974376e3
Thinley <thinleylama44@gmail.com>
2024-02-09 22:56:33 +0530

    Nav done

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,27 +1,30 @@
+import cart from '../assets/cart.svg'
+import world from '../assets/world.svg'
+
 export default function Header() {
     return (
-        <div className="flex justify-around shadow h-16 items-center">
+        <div className="flex shadow h-16 items-center justify-around">
 
-          <div className="flex gap-11 items-center">
-
-            <div>
-                <img className="h-8" src="https://www.udemy.com/staticx/udemy/images/v7/logo-udemy.svg" alt="logo" />
-            </div>
-            <div>Catagories</div>
-            <div>
-             <input type="search" 
-               id="default-search" 
-               className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-3xl bg-gray-50 focus:ring-blue-500 focus:border-blue-500" 
-               placeholder="Search for anything" required />
-            </div>
-            <div>Udemy Business</div>
-            <div>Teach On Udemy</div>
-            <div>cart logo</div>
-            <div>Log in</div>
-            <div>Sign up</div>
-            <div>language logo</div>
-
-          </div>
+              <div className='cursor-pointer'>
+                  <img className="h-8" src="https://www.udemy.com/staticx/udemy/images/v7/logo-udemy.svg" alt="logo" />
+              </div>
+              <div className='cursor-pointer'>Catagories</div>
+              <div className="w-5/12">
+                <input type="search" 
+                  id="default-search" 
+                  className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-3xl bg-gray-50 focus:ring-blue-500 focus:border-blue-500" 
+                  placeholder="Search for anything" required />
+              </div>
+              <div className='cursor-pointer'>Udemy Business</div>
+              <div className='cursor-pointer'>Teach On Udemy</div>
+              <div className='cursor-pointer'>
+                <img width="25px" src={cart} alt="cart" />
+              </div>
+              <div className='cursor-pointer font-bold border border-black py-2 px-4 hover:bg-slate-100'>Log in</div>
+              <div className='cursor-pointer font-bold bg-black text-white border border-black py-2 px-4 hover:bg-slate-800'>Sign up</div>
+              <div className='cursor-pointer border border-black p-2'>
+                <img width="25px" src={world} alt="world" />
+              </div>
 
         </div>
     )
