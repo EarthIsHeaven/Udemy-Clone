ce8f4f143e8b110c3f707487d3a37cc614b8c110
Thinley <thinleylama44@gmail.com>
2024-02-09 19:23:22 +0530

    Footer working

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -3,8 +3,28 @@ export default function Footer() {
     return(
         <div className="bg-black text-white">
             <div className="flex justify-between">
-                <div>
-                    text
+                <div className="flex">
+                    <div>
+                        <div>Udemy business</div>
+                        <div>Teach on Udemy</div>
+                        <div>Get the app</div>
+                        <div>About us</div>
+                        <div>Contact us</div>
+                    </div>
+                    <div>
+                        <div>Careers</div>
+                        <div>Blog</div>
+                        <div>Help and Support</div>
+                        <div>Affiliate</div>
+                        <div>Investors</div>
+                    </div>
+                    <div>
+                        <div>Terms</div>
+                        <div>Privacy policy</div>
+                        <div>Cookie settings</div>
+                        <div>Sitemap</div>
+                        <div>Assessibility statement</div>
+                    </div>
                 </div>
                 <div>
                     English
